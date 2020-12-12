ed5809f6af95af8a3c8672f0ad850013622d61d6
vlad_veko <veko.2001@gmail.com>
2020-12-12 21:09:10 +0300

    Added get article command

---
+++ src/main/java/com/blog/controller/command/impl/GetArticle.java
@@ -0,0 +1,40 @@
+package com.blog.controller.command.impl;
+
+import com.blog.bean.Article;
+import com.blog.controller.command.Command;
+import com.blog.service.BlogService;
+import com.blog.service.ServiceProvider;
+import com.blog.service.exception.ServiceException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class GetArticle implements Command {
+
+    private static final String ARTICLE_ID = "articleID";
+    private static final String REDIRECT = "WEB-INF/jsp/article";
+    private static final String REDIRECT_ERROR = "";
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        HttpSession session = request.getSession(true);
+        ServiceProvider serviceProvider = ServiceProvider.getInstance();
+        BlogService blogService = serviceProvider.getBlogService();
+
+        try{
+            long articleID = Integer.parseInt(request.getParameter(ARTICLE_ID));
+            Article article = blogService.getArticle(articleID);
+
+            session.setAttribute("article", article);
+            response.sendRedirect(REDIRECT);
+        }
+        catch (ServiceException ex){
+            response.sendRedirect(REDIRECT_ERROR);
+        }
+
+    }
+}
--- src/main/java/com/blog/dao/ArticalDAO.java
+++
@@ -1,9 +0,0 @@
-package com.blog.dao;
-
-import com.blog.bean.Article;
-
-public interface ArticalDAO {
-    void addArtical(Article article);
-    void deleteArtical(long idArtical);
-    void delete(Article article);
-}
